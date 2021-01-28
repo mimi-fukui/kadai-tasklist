0903f4623d30ef46691b8f767d364ceaf8eed568
m.fukui <fukuinorecruit@gmail.com>
2021-01-28 12:34:42 +0000

    kadai-tasklist

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+    root to: 'tasks#index'
+    
+    resources :tasks
+    # get 'tasks/:id', to: 'tasks#show'
+    # post 'tasks', to: 'tasks#create'
+    # put 'tasks/:id', to: 'tasks#update'
+    # delete 'tasks/:id', to: 'tasks#destroy'
+    
+    # get 'tasks' to: 'tasks#show'
+    
+    # get 'tasks/new', to 'tasks#new'
+    
+    # get 'tasks/:id/edit', to: 'tasks#edit'
 end
