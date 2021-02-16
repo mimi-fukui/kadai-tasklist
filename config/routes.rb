8e0f0d7628685146ffd0eabd08364c0304d724f9
m.fukui <fukuinorecruit@gmail.com>
2021-02-16 14:47:11 +0000

    clean routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-  get 'sessions/create'
-  get 'sessions/destroy'
+#  get 'sessions/new'
+#  get 'sessions/create'
+#  get 'sessions/destroy'
 #  get 'users/index'
 #  get 'users/show'
 #  get 'users/new'
