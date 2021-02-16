239cac432f929156b83e6180c99bd4b1511ac6a6
m.fukui <fukuinorecruit@gmail.com>
2021-02-16 13:33:28 +0000

    patch correct user to show, edit, update

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,6 @@
 class TasksController < ApplicationController
     before_action :require_user_logged_in
-    before_action :correct_user, only: [:destroy]
+    before_action :correct_user, only: [:destroy, :edit, :update, :show]
     
     def index
         if logged_in?
@@ -10,7 +10,7 @@ class TasksController < ApplicationController
     end
     
     def show
-        @task = Task.find(params[:id])
+       #@task = Task.find(params[:id])
     end
     
     def new
@@ -31,11 +31,11 @@ class TasksController < ApplicationController
     end
     
     def edit
-        @task = Task.find(params[:id])
+        #@task = Task.find(params[:id])
     end
     
     def update
-        @task = Task.find(params[:id])
+        #@task = Task.find(params[:id])
         
         if @task.update(task_params)
             flash[:success] = 'Taskは正常に更新されました'
